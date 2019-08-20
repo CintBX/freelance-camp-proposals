58ef59368c23b4589e7ab8f2cc2b66044f5c17d7
Dustin Cintron <D.Cint88@gmail.com>
2019-08-20 01:45:27 -0400

    set the data url as root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :proposals
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'proposals#index'
 end
